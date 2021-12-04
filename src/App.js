0e13c65271f9ab1351bb7bc13b1a8a4a4c8a54a4
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2021-12-04 13:21:24 +0100

    fixed spelling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         </h1>
         <footer>This project was coded by {" "} 
         <a href="https://clever-varahamihira-8d6483.netlify.app" target="_blank" rel="noreferrer"> Kim Wauben </a>
-        an is {" "}
+        and is {" "}
         <a href="https://github.com/KimW1998/react-weather-app-final" target="_blank" rel="noreferrer"> Open-sourced on GitHub</a>
         </footer>
     </div>
