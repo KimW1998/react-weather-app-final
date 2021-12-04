d8ae2e7324a52884fc0008cdd234621d919f7d72
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2021-12-04 13:34:10 +0100

    created and imported Weather file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>
-          Weather App
-        </h1>
+        <Weather />
         <footer>This project was coded by {" "} 
         <a href="https://clever-varahamihira-8d6483.netlify.app" target="_blank" rel="noreferrer"> Kim Wauben </a>
         and is {" "}
